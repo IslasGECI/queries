91e363c6b9e1127a62ae9463826147f0bd4ade52
Maritza <maritza.bello@islas.org.mx>
2020-11-25 11:34:35 -0800

    :white_check_mark: :sparkles: Agrega prueba de query

---
+++ tests/bats_tests/test_select_growth_rates_and_p_values.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bats
+
+@test "select_growth_rates_and_p_values" {
+  run cat tests/data/output_test_select_growth_rates_and_p_vales.csv
+  result="$(./src/select_growth_rates_and_p_values tests/data/cormorant_all_islets_growth_rates.csv p_value "<= 0.1")"
+  [ "$output" = "$result" ]
+}
