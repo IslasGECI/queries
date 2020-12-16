4ee0c38c7bd4021b3da04e95d395daa4f035c65f
Maritza <maritza.bello@islas.org.mx>
2020-12-16 13:02:16 -0800

    🚚 Cambia nombres de archivos de prueba y modifica prueba

--- tests/bats_tests/test_select_growth_rates_and_p_values.sh
+++ tests/bats_tests/test_select_growth_rates_and_p_values.sh
@@ -7,8 +7,8 @@
 }
 
 @test "select_growth_rates_and_p_values_seabirds" {
-  run cat tests/data/output_test_selecit_growth_rates_and_p_values_seabirds.csv
-  result="$(./src/select_growth_rates_and_p_values tests/data/seabirds_all_islets_growth_rates.csv p_value "<= 0.1" 2)"
+  run cat tests/data/output_test_select_growth_rates_and_p_values_seabirds.csv
+  result="$(./src/select_growth_rates_and_p_values tests/data/seabirds_growth_rates.csv p_value "<= 0.1" 2)"
   [ "$output" = "$result" ]
 }
 
