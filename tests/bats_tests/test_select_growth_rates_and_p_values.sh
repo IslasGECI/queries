bc1ccadaf1bcee44816600afc8d00dcb1a3f5191
Maritza <maritza.bello@islas.org.mx>
2020-12-16 13:26:48 -0800

    🥅 Corrige archivo de salida de prueba 🧐

--- tests/bats_tests/test_select_growth_rates_and_p_values.sh
+++ tests/bats_tests/test_select_growth_rates_and_p_values.sh
@@ -6,14 +6,15 @@
   [ "$output" = "$result" ]
 }
 
-@test "select_growth_rates_and_p_values_seabirds" {
-  run cat tests/data/output_test_select_growth_rates_and_p_values_seabirds.csv
-  result="$(./src/select_growth_rates_and_p_values tests/data/seabirds_growth_rates.csv p_value "<= 0.1" 2)"
+@test "select_growth_rates_and_p_values_regional_seabirds" {
+  run cat tests/data/output_test_select_growth_rates_and_p_values_regional_seabirds.csv
+  result="$(./src/select_growth_rates_and_p_values tests/data/seabirds_all_islets_growth_rates.csv p_value "<= 0.1" 2)"
   [ "$output" = "$result" ]
 }
 
-@test "select_growth_rates_and_p_values_regional_seabirds" {
-  run cat tests/data/output_test_select_growth_rates_and_p_values_regional_seabirds.csv
-  result="$(./src/select_growth_rates_and_p_values tests/data/seabirds_all_islets_growth_rates.csv p_value "<= 0.1" 3)"
+@test "select_growth_rates_and_p_values_seabirds" {
+  run cat tests/data/output_test_select_growth_rates_and_p_values_seabirds.csv
+  result="$(./src/select_growth_rates_and_p_values tests/data/seabirds_growth_rates.csv p_value "<= 0.1" 3)"
   [ "$output" = "$result" ]
 }
+
