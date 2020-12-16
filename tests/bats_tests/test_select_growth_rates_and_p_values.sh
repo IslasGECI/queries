8b163939332828bb0cd8cb8bb9407da472bd9ea4
Maritza <maritza.bello@islas.org.mx>
2020-12-16 10:00:48 -0800

    ⚡️Modifica prueba para poderle indicar la opción de las columnas ⚡️

--- tests/bats_tests/test_select_growth_rates_and_p_values.sh
+++ tests/bats_tests/test_select_growth_rates_and_p_values.sh
@@ -2,6 +2,6 @@
 
 @test "select_growth_rates_and_p_values" {
   run cat tests/data/output_test_select_growth_rates_and_p_vales.csv
-  result="$(./src/select_growth_rates_and_p_values tests/data/cormorant_all_islets_growth_rates.csv p_value "<= 0.1")"
+  result="$(./src/select_growth_rates_and_p_values tests/data/cormorant_all_islets_growth_rates.csv p_value "<= 0.1" 1)"
   [ "$output" = "$result" ]
 }
