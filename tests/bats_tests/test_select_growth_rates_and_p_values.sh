897fcd625f82e199d08a1fdc77998d3261da9bbb
Maritza <maritza.bello@islas.org.mx>
2020-12-16 13:34:22 -0800

    🚧 Modifica prueba y agrega archivos

--- tests/bats_tests/test_select_growth_rates_and_p_values.sh
+++ tests/bats_tests/test_select_growth_rates_and_p_values.sh
@@ -8,7 +8,7 @@
 
 @test "select_growth_rates_and_p_values_regional_seabirds" {
   run cat tests/data/output_test_select_growth_rates_and_p_values_regional_seabirds.csv
-  result="$(./src/select_growth_rates_and_p_values tests/data/seabirds_all_islets_growth_rates.csv p_value "<= 0.1" 2)"
+  result="$(./src/select_growth_rates_and_p_values tests/data/regional_seabirds_growth_rates.csv p_value "<= 0.1" 2)"
   [ "$output" = "$result" ]
 }
 
@@ -17,4 +17,3 @@
   result="$(./src/select_growth_rates_and_p_values tests/data/seabirds_growth_rates.csv p_value "<= 0.1" 3)"
   [ "$output" = "$result" ]
 }
-
