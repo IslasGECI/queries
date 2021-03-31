358c582e3d258c3227cc6c9548ce191a66668fe0
Maritza <maritza.bello@islas.org.mx>
2021-03-31 11:20:39 -0700

    ⚗ Agrega experimentos con la documentación de Cli

--- query_cli/solicitudes.py
+++ query_cli/solicitudes.py
@@ -10,14 +10,17 @@ def cli():
 @cli.command()
 @click.option("--data_file", "-d", type=click.Path(),
     help="Nombre del recurso csv")
-@click.option("--p_value", "-p", type=click.Path(),
+@click.option("--p_value", "-p", type=str, metavar="<p-value>",
     help="Columna del p-value")
-@click.option("--conditional", "-c", type=click.Path(),
+@click.option("--conditional", "-c", type=str,
     help="Condicional para el p-value")
-@click.option("--fields", "-f", type=click.Path(),
+@click.option("--fields", "-f", type=int,
     help="Opción que corresponde a las columnas que se van a seleccionar")
 def select_growth_rates_and_p_values(data_file,p_value,conditional,fields):
     os.system("./src/select_growth_rates_and_p_values {} {} {} {}".format(data_file,p_value,conditional,fields))
+    """
+    hola
+    """
     pass
 
 
